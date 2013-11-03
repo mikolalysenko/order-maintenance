424e56c98442b6fdd0f7c3c2e59fbfeccc9f8500
Mikola Lysenko <mikolalysenko@gmail.com>
2013-11-03 09:20:39 -0600

    adding test cases for remove()

--- test/list.js
+++ test/list.js
@@ -3,18 +3,50 @@
 module.exports = testList
 
 function testList(createList, t) {
+
+  //Basic insert and compare
   var a = createList()  
   var b = a.insert()
+  t.equals(a.next, b)
+  t.equals(b.prev, a)
   t.ok(a.compare(b) < 0)
   t.ok(b.compare(a) > 0)
   t.ok(a.compare(a) === 0)
   
-  var l = createList()
+  //More insertions
+  var base = createList()
   for(var i=0; i<1000; ++i) {
-    l.insert()
+    base.insert()
   }
-  var base = l
-  l = l.next
+  var l = base.next
+  while(l) {
+    t.ok(l.prev.compare(l) < 0)
+    t.ok(l.compare(l.prev) > 0)
+    t.ok(l.compare(l) === 0)
+    t.ok(l.compare(base) > 0)
+    t.ok(base.compare(l) < 0)
+    l = l.next
+  }
+
+  //Try remove
+  for(var i=0; i<500; ++i) {
+    base.next.remove()
+  }
+  l = base.next
+  while(l) {
+    t.ok(l.prev.compare(l) < 0)
+    t.ok(l.compare(l.prev) > 0)
+    t.ok(l.compare(l) === 0)
+    t.ok(l.compare(base) > 0)
+    t.ok(base.compare(l) < 0)
+    l = l.next
+  }
+
+  //Try inserting again
+  for(var i=0; i<1000; ++i) {
+    base.insert()
+  }
+  l = base.next
   while(l) {
     t.ok(l.prev.compare(l) < 0)
     t.ok(l.compare(l.prev) > 0)
